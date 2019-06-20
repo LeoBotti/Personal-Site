cd9b8e2ce8548023027df9914d8e2ecf9735814b
Nicholas Chan <nicholaschan1994@hotmail.com>
2019-06-20 00:25:29 -0700

    Add styling and easter egg to nav brand

--- src/components/About.js
+++ src/components/About.js
@@ -4,9 +4,7 @@ const About = (props) => {
   return(
     <div className="About" id='about'>
       <h2>ABOUT ME</h2>
-      Hi, I’m Nicholas! I’m a Full-Stack web developer based in Vancouver, BC. 
-      programming
-      educationtional background
+      Hi, I’m Nicholas! I’m a Full-Stack web developer based in Vancouver, BC.
     </div>
   )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,7 +6,13 @@ import { Navbar, NavbarBrand, Nav } from 'react-bootstrap'
 const NavBar = (props) => {
   return(
     <Navbar bg='dark' variant='dark'>
-      <NavbarBrand className='mr-auto' href='/'>Nicholas A. Chan</NavbarBrand>
+      <NavbarBrand className='nav-initials' style={{marginRight: 0}} href='/'>N.</NavbarBrand>
+      <NavbarBrand style={{marginRight: 0, color: "#343a40"}} target="_blank" href='http://www.nyan.cat/index.php?cat=technyancolor'>Y</NavbarBrand>
+      <NavbarBrand className='nav-initials' style={{marginRight: 0}} href='/'>A.</NavbarBrand>
+      <NavbarBrand style={{marginRight: 0, color: "#343a40"}} target="_blank" href='http://www.nyan.cat/index.php?cat=technyancolor'>N</NavbarBrand>
+      <NavbarBrand className='nav-initials' style={{marginRight: 0}} href='/'>C.</NavbarBrand>
+      <NavbarBrand style={{marginRight: 0, color: "#343a40"}} target="_blank" href='http://www.nyan.cat/index.php?cat=technyancolor'>A</NavbarBrand>
+      <NavbarBrand style={{color: "#343a40"}}className='mr-auto' target="_blank" href='http://www.nyan.cat/index.php?cat=technyancolor'>T</NavbarBrand>
 
       <Nav className='mr-sm-2'>
         <NavLink className='navlink' to="/">Home</NavLink>
@@ -14,7 +20,6 @@ const NavBar = (props) => {
         <NavHash className='navlink' to="/#webdev">Web Dev</NavHash>
         <NavLink className='navlink' to="/projects">Projects</NavLink>
       </Nav>
-
     </Navbar>
   )
 }
