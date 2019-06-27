98214883e692c81f321c8eaf9eab87787b6977c1
Nicholas Chan <nicholaschan1994@hotmail.com>
2019-06-26 22:19:41 -0700

    Edit About Me

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,12 @@ const About = (props) => {
   return(
     <div className="About" id='about'>
       <h2>ABOUT ME</h2>
-      Hi, I’m Nicholas! I’m a Full-Stack web developer based in Vancouver, BC.
+      Hi, I’m Nicholas! I’m a Full-Stack web developer based in Vancouver, BC. <br/>
+      I’m passionate about writing functional and efficient code that creates an intuitive user experience. <br/>
+      On top of building web apps, I also love to build PCs for others. 
+      <br/> Having grown up in Hong Kong and studied at an international school for 12 years, and 5 years here in Vancouver, 
+      <br/> I've been heavily exposed to both western and eastern cultures, which is why I speak 3 languages (English, Cantonese, Mandarin) 
+      <br/> and am adaptable and welcoming to experience different cultures and connect with people of different backgrounds.
     </div>
   )
 }
