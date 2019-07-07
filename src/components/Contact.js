80b17c66402f74e07d47d3aeaded5248bdc735a7
Nicholas Chan <nicholaschan1994@hotmail.com>
2019-07-06 20:17:24 -0700

    change email address in Contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,7 @@ const Contact = (props) => {
   return(
     <div className="Contact" id='contact'>
       <h2>CONTACT</h2>
-      EMAIL: <a href="mailto:nicholaschan1994@gmail.com">nicholaschan1994@gmail.com</a> <br/>
+      EMAIL: <a href="mailto:me@nicachan.dev">me@nicachan.dev</a> <br/>
       GITHUB: <a href="https://github.com/NicAChan" target="_blank" rel="noopener noreferrer">github.com/NicAChan</a> <br/>
       LINKEDIN: <a href="https://linkedin.com/in/nicholas-a-chan" target='_blank' rel="noopener noreferrer">linkedin.com/in/nicholas-a-chan</a>
     </div>
