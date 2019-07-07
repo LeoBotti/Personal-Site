ca2c9ba48d918f8058a67c53161484071eaf0ff0
Nicholas Chan <nicholaschan1994@hotmail.com>
2019-07-06 19:57:39 -0700

    Add basename property to BrowserRouter

--- src/components/App.js
+++ src/components/App.js
@@ -29,7 +29,7 @@ class App extends Component {
 
     return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename="/Personal-Site/">
         <NavBar />
 
         <Switch>
