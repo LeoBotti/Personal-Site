4b4c109973eb6b5423627da9e33ce1ae0c6da168
Nicholas Chan <nicholaschan1994@hotmail.com>
2019-07-08 17:59:53 -0700

    Styling and change BrowswerRouter basename

--- src/components/App.js
+++ src/components/App.js
@@ -29,7 +29,7 @@ class App extends Component {
 
     return (
     <div className="App">
-      <BrowserRouter basename="/Personal-Site/">
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <NavBar />
 
         <Switch>
