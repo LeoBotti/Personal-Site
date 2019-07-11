e69efc8b039fbac50f3e97e97a9140835031c1cc
Nicholas Chan <nicholaschan1994@hotmail.com>
2019-07-11 02:10:46 -0700

    remove basename from BrowserRouter

--- src/components/App.js
+++ src/components/App.js
@@ -29,7 +29,7 @@ class App extends Component {
 
     return (
     <div className="App">
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <BrowserRouter>
         <NavBar />
 
         <Switch>
